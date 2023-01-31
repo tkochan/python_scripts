f25babd0ebf69822cff23e00a458547002442557
Travis Kochan <36889283+tkochan@users.noreply.github.com>
2023-01-31 13:46:19 -0600

    Update Primer_design.py

--- Primer_design.py
+++ Primer_design.py
@@ -4,7 +4,9 @@
 #2) extracts 700 base pairs upstream and 700 base pairs downstream of the gene
 #3) designs primers to amplify those two fragments using primer3
 #4) adds the necessary over hangs for gibson cloning
-#5) outputs all of the designed primers with overhangs to a CSV
+#5) designs confirmation primers that are designed to be outside of the cloning primers
+#6) outputs all of the designed primers with overhangs to a CSV
+#7) outputs the fragment (upstream-apramycin cassette-downstream) of DNA these primers are meant to amplify and clone as a variable called complete
 
 #Dependencies
 #install primer3
