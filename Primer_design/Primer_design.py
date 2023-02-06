8284a791e8078021990f9bef838e0e2922ae25d2
Travis Kochan <36889283+tkochan@users.noreply.github.com>
2023-02-06 11:47:04 -0600

    Update Primer_design.py

--- Primer_design/Primer_design.py
+++ Primer_design/Primer_design.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
-#This is python script to design primers to knockout a gene included in a fastA file from a genome in fastA file.
+#This is python script to design primers to knockout a gene included in file genes.txt from a strain included in file genome.fasta.
 #1) It blasts the gene against the genome, outputs the exact allele that the strain has.
 #2) extracts 700 base pairs upstream and 700 base pairs downstream of the gene
 #3) designs primers to amplify those two fragments using primer3
 #4) adds the necessary over hangs for gibson cloning
 #5) designs confirmation primers that are designed to be outside of the cloning primers
 #6) outputs all of the designed primers with overhangs to a CSV
-#7) outputs the fragment (upstream-apramycin cassette-downstream) of DNA these primers are meant to amplify and clone as a variable called complete
-#8) format should be python3 Primer_design.py {gene fastA file} {genome fastA file}
+#7) for Klebsiella outputs the fragment (upstream-apramycin cassette-downstream) of DNA these primers are meant to amplify and clone as a variable called complete
+#8) for Pseudomonas outputs the fragment
 
 #Dependencies
 #install primer3
 #install blast
+#install biopython
+
 
 import os
 import sys
@@ -22,7 +24,14 @@ import csv
 # Get the input filenames from the command line
 gene = sys.argv[1]
 genome = sys.argv[2]
+species = sys.argv[3]
+plasmid = sys.argv[4]
+size =  sys.argv[5]
+MAX = sys.argv[6]
 
+size = int(size) + 200
+maxsize = int(size) + 300
+MAX = int(MAX)
 # Create the BLAST database for the genome
 os.system(f"makeblastdb -in {genome} -out tmp -dbtype nucl > /dev/null")
 
@@ -87,11 +96,11 @@ with open(genome, "r") as genome_file:
                 if gene in str(genome_record.seq):
                     gene_start = str(genome_record.seq).index(gene)
                     gene_end = gene_start + len(gene)
-                    if gene_start-700>0:
-                        upstream = genome_record.seq[gene_start-700:gene_start-0]
+                    if gene_start-size>0:
+                        upstream = genome_record.seq[gene_start-size:gene_start-0]
                     else:
                         upstream = genome_record.seq[0:gene_start-0]
-                    downstream = genome_record.seq[gene_end:gene_end+700]
+                    downstream = genome_record.seq[gene_end:gene_end+size]
 
                     # Write the upstream and downstream sequences to FASTA files
                     with open("upstream.fasta", "w") as upstream_file:
@@ -111,27 +120,35 @@ with open(genome, "r") as genome_file:
                 if gene in str(genome_record.seq):
                     gene_start = str(genome_record.seq).index(gene)
                     gene_end = gene_start + len(gene)
-                    if gene_start-1000>0:
-                        upstream2 = genome_record.seq[gene_start-1000:gene_start-0]
+                    if gene_start-maxsize>0:
+                        upstream2 = genome_record.seq[gene_start-maxsize:gene_start-0]
                     else:
                         upstream2 = genome_record.seq[0:gene_start-0]
-                    downstream2 = genome_record.seq[gene_end:gene_end+1000]
+                    downstream2 = genome_record.seq[gene_end:gene_end + maxsize]
+x = int(size -250)
+y = int(size -50)
 # Set global parameters for primer3
 global_args = {
-    'PRIMER_PRODUCT_SIZE_RANGE': [450,650],
+    'PRIMER_PRODUCT_SIZE_RANGE': [x,y],
     'PRIMER_MIN_SIZE': 25,
     'PRIMER_MAX_SIZE': 25,
     'PRIMER_OPT_SIZE': 25,
     'PRIMER_NUM_RETURN': 1,
-    'PRIMER_EXPLAIN_FLAG': '1'
+    'PRIMER_EXPLAIN_FLAG': '1',
+    'PRIMER_MAX_TM': MAX
 }
+x = int(2*maxsize*0.7+1)
+print(x)
+y = int(2*maxsize*0.9)
+print(y)
 global_args2 = {
-    'PRIMER_PRODUCT_SIZE_RANGE': [1401,1800],
+    'PRIMER_PRODUCT_SIZE_RANGE': [x,y],
     'PRIMER_MIN_SIZE': 15,
     'PRIMER_MAX_SIZE': 25,
     'PRIMER_OPT_SIZE': 20,
     'PRIMER_NUM_RETURN': 1,
     'PRIMER_EXPLAIN_FLAG': '1'
+
 }
 # Read upstream and downstream sequences from FASTA files
 with open('upstream.fasta', 'r') as f:
@@ -169,6 +186,7 @@ seq_args3 = {
 primer_results1 = primer3.bindings.designPrimers(seq_args1, global_args,)
 for i in range(primer_results1['PRIMER_PAIR_NUM_RETURNED']):
     left_seq = primer_results1['PRIMER_LEFT_{}_SEQUENCE'.format(i)]
+    
 
 primer_results2 = primer3.bindings.designPrimers(seq_args2, global_args,)
 
@@ -183,52 +201,107 @@ for i in range(primer_results3['PRIMER_PAIR_NUM_RETURNED']):
     primer7 = primer_results3['PRIMER_LEFT_{}_SEQUENCE'.format(i)]
 #Apramycin sequence
 apramycin = "tgtaggctggagctgcttcgaagttcctatactttctagagaataggaacttcggaataggaacttatgagctcagccaatcgactggcgagcggcatcgcattcttcgcatcccgcctctggcggatgcaggaagatcaacggatctcggcccagttgacccagggctgtcgccacaatgtcgcgggagcggatcaaccgagcaaaggcatgaccgactggaccttccttctgaaggctcttctccttgagccacctgtccgccaaggcaaagcgctcacagcagtggtcattctcgagataatcgacgcgtaccaacttgccatcctgaagaatggtgcagtgtctcggcaccccatagggaacctttgccatcaactcggcaagatgcagcgtcgtgttggcatcgtgtcccacgccgaggagaagtacctgcccatcgagttcatggacacgggcgaccgggcttgcaggcgagtgaggtggcaggggcaatggatcagagatgatctgctctgcctgtggccccgctgccgcaaaggcaaatggatgggcgctgcgctttacatttggcaggcgccagaatgtgtcagagacaactccaaggtccggtgtaacgggcgacgtggcaggatcgaacggctcgtcgtccagacctgaccacgagggcatgacgagcgtccctcccggacccagcgcagcacgcagggcctcgatcagtccaagtggcccatcttcgaggggccggacgctacggaaggagctgtggaccagcagcacaccgccgggggtaaccccaaggttgagaagctgaccgatgagctcggcttttcgccattcgtattgcacgacattgcactccaccgctgatgacatcagtcgatcatagcacgatcaacggcactgttgcaaatagtcggtggtgataaacttatcatccccttttgctgatggagctgcattcaaaggccggcattttcagcgtgacatcattctgtgggccgtacgctggtactgcaaatacggcatcagttaccgtgagctgcattttccgctgcataaccctgcttcggggtcattatagcgattttttcggtatatccatcctttttcgcacgatatacaggattttgccaaagggttcgtgtagactttccttggtgtatccaacggcgtcagccgggcaggataggtgaagtaggcccacccgcgagcgggtgttccttcttcactgtcccttattcgcacctggcggtgctcaacgggaatcctgctctgcgaggctggcgggaacttcgaagttcctatactttctagagaataggaacttcgaactgcaggtcgacggatccccggaa"
-#Define Primers
-primer1 = left_seq
-primer2 = upstream_seq[-25:].translate(str.maketrans("ATGC", "TACG"))[::-1]
-primer3 = "TGTAGGCTGGAGCTGCTTCGAAGTT"
-primer4 = "TTCCGGGGATCCGTCGACCTGCAGT"
-primer5 = downstream_seq[:25]
-primer6 = right_seq
+
+if species == "Kp":
+    #Define Primers
+    primer1 = left_seq
+    primer2 = upstream_seq[-25:].translate(str.maketrans("ATGC", "TACG"))[::-1]
+    primer3 = "TGTAGGCTGGAGCTGCTTCGAAGTT"
+    primer4 = "TTCCGGGGATCCGTCGACCTGCAGT"
+    primer5 = downstream_seq[:25]
+    primer6 = right_seq
+
+    #Define Overhangs
+    primer1_overhang = "CCAAGCTTCTCGAGG"
+    primer2_overhang = "CAGCTCCAGCCTACA"
+    primer3_overhang = upstream_seq[-15:]
+    primer4_overhang = downstream_seq[:15].translate(str.maketrans("ATGC", "TACG"))[::-1]
+    primer5_overhang = "GACGGATCCCCGGAA"
+    primer6_overhang = "CGGGCTGCAGGAATT"
+
+    #Add Overhangs
+    primer1 = primer1_overhang + primer1
+    primer3 = primer3_overhang + primer3
+    primer2 = primer2_overhang + primer2
+    primer5 = primer5_overhang + primer5
+    primer4 = primer4_overhang + primer4
+    primer6 = primer6_overhang + primer6
+
+    complete=">Complete\n"
+    complete = str(complete+upstream2+apramycin+downstream2)
+    with open("complete.fasta", "w") as complete_file:
+        complete_file.write(str(complete))
+
+    primers = [
+        {"Primer": "Primer 1", "Sequence": primer1},
+        {"Primer": "Primer 2", "Sequence": primer2},
+        {"Primer": "Primer 3", "Sequence": primer3},
+        {"Primer": "Primer 4", "Sequence": primer4},
+        {"Primer": "Primer 5", "Sequence": primer5},
+        {"Primer": "Primer 6", "Sequence": primer6},
+        {"Primer": "Primer 7", "Sequence": primer7},
+        {"Primer": "Primer 8", "Sequence": primer8},
+    ]
+
+    with open("primers.csv", "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["Primer", "Sequence"])
+        writer.writeheader()
+        for primer in primers:
+            writer.writerow(primer)
+
+if species == "Pa":
+    print("the species is " + species)
+    aligned_gene = str(aligned_gene)
+    #Define Primers
+    primer1 = left_seq
+    primer2 = aligned_gene[:6] + upstream_seq[-19:]
+    primer2 = primer2.translate(str.maketrans("ATGC", "TACG"))[::-1]
+    primer3 = aligned_gene[-6:] + downstream_seq[:19]
+    primer4 = right_seq
+    primer5 = primer7
+    primer6 = primer8
+
+    if plasmid == "1":
+        #Define Overhangs
+        primer1_overhang = "GCATAAATGTAAAGCA"
+        primer2_overhang = "AACTCGAGCCGCAAGCATGCTGAA"
+        primer3_overhang = "TTCAGCATGCTTGCGGCTCGAGTT"
+        primer4_overhang = "CTAGAGTCGACCTGCAGA"
+
+    if plasmid == "2":
+        #Define Overhangs
+        primer1_overhang = "GTAAAACGACGGCCAGTGCCA"
+        primer2_overhang = "AACTCGAGCCGCAAGCATGCTGAA"
+        primer3_overhang = "TTCAGCATGCTTGCGGCTCGAGTT"
+        primer4_overhang = "GTCGACCTGCAGGCATGCA"
 
 
-#Define Overhangs
-primer1_overhang = "CCAAGCTTCTCGAGG"
-primer2_overhang = "CAGCTCCAGCCTACA"
-primer3_overhang = upstream_seq[-15:]
-primer4_overhang = downstream_seq[:15].translate(str.maketrans("ATGC", "TACG"))[::-1]
-primer5_overhang = "GACGGATCCCCGGAA"
-primer6_overhang = "CGGGCTGCAGGAATT"
+    #Add Overhangs
+    primer1 = primer1_overhang + primer1
+    primer2 = primer2_overhang + primer2
+    primer3 = primer3_overhang + primer3
+    primer4 = primer4_overhang + primer4
 
 
-#Add Overhangs
-primer1 = primer1_overhang + primer1
-primer3 = primer3_overhang + primer3
-primer2 = primer2_overhang + primer2
-primer5 = primer5_overhang + primer5
-primer4 = primer4_overhang + primer4
-primer6 = primer6_overhang + primer6
 
+    complete=">Complete\n"
+    scar="AACTCGAGCCGCAAGCATGCTGAA"
+    complete = str(complete+upstream2+ scar + downstream2)
+    with open("complete.fasta", "w") as complete_file:
+        complete_file.write(str(complete))
 
-complete=">Complete\n"
-complete = str(complete+upstream2+apramycin+downstream2)
-with open("complete.fasta", "w") as complete_file:
-    complete_file.write(str(complete))
+    primers = [
+        {"Primer": "Primer 1", "Sequence": primer1},
+        {"Primer": "Primer 2", "Sequence": primer2},
+        {"Primer": "Primer 3", "Sequence": primer3},
+        {"Primer": "Primer 4", "Sequence": primer4},
+        {"Primer": "Primer 5", "Sequence": primer5},
+        {"Primer": "Primer 6", "Sequence": primer6},
 
-primers = [
-    {"Primer": "Primer 1", "Sequence": primer1},
-    {"Primer": "Primer 2", "Sequence": primer2},
-    {"Primer": "Primer 3", "Sequence": primer3},
-    {"Primer": "Primer 4", "Sequence": primer4},
-    {"Primer": "Primer 5", "Sequence": primer5},
-    {"Primer": "Primer 6", "Sequence": primer6},
-    {"Primer": "Primer 7", "Sequence": primer7},
-    {"Primer": "Primer 8", "Sequence": primer8},
-]
+    ]
 
-with open("primers.csv", "w", newline="") as f:
-    writer = csv.DictWriter(f, fieldnames=["Primer", "Sequence"])
-    writer.writeheader()
-    for primer in primers:
-        writer.writerow(primer)
-
+    with open("primers.csv", "w", newline="") as f:
+        writer = csv.DictWriter(f, fieldnames=["Primer", "Sequence"])
+        writer.writeheader()
+        for primer in primers:
+            writer.writerow(primer)
